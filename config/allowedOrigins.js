bf13d2788b87fa6aa680d711a5ccec2da37d64a7
Abdul Momin <66585914+abdulmomin956@users.noreply.github.com>
2022-07-01 19:06:58 +0600

    cors origin update

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
-
+    'https://polite-whistler-22718.herokuapp.com',
+    'https://power-hack-17946.web.app',
     'http://127.0.0.1:5500',
     'http://localhost:5500',
     'http://127.0.0.1:3000',
