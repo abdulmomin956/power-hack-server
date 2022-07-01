82d8771de81129c204234cbd8802768b2312b39f
Abdul Momin <66585914+abdulmomin956@users.noreply.github.com>
2022-07-01 23:35:37 +0600

    login with mongodb

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,46 +1,53 @@
-const usersDB = {
-    users: require('../model/users.json'),
-    setUsers: function (data) { this.users = data }
-}
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PWD}@cluster0.sn77ciu.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 const bcrypt = require('bcrypt');
 
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
-const fsPromises = require('fs').promises;
-const path = require('path');
+
 
 const handleLogin = async (req, res) => {
     const { user, pass } = req.body;
     if (!user || !pass) return res.status(400).json({ 'message': 'Username and password are required.' });
-    const foundUser = usersDB.users.find(person => person.username === user);
-    if (!foundUser) return res.sendStatus(401); //Unauthorized 
-    // evaluate password 
-    const match = await bcrypt.compare(pass, foundUser.password);
-    if (match) {
-        // create JWTs
-        const accessToken = jwt.sign(
-            { "username": foundUser.username },
-            process.env.ACCESS_TOKEN_SECRET,
-            { expiresIn: '30s' }
-        );
-        const refreshToken = jwt.sign(
-            { "username": foundUser.username },
-            process.env.REFRESH_TOKEN_SECRET,
-            { expiresIn: '1d' }
-        );
-        // Saving refreshToken with current user
-        const otherUsers = usersDB.users.filter(person => person.username !== foundUser.username);
-        const currentUser = { ...foundUser, refreshToken };
-        usersDB.setUsers([...otherUsers, currentUser]);
-        await fsPromises.writeFile(
-            path.join(__dirname, '..', 'model', 'users.json'),
-            JSON.stringify(usersDB.users)
-        );
-        res.cookie('jwt', refreshToken, { httpOnly: true, sameSite: 'None', secure: true, maxAge: 24 * 60 * 60 * 1000 });
-        res.json({ accessToken });
-    } else {
+    try {
+        await client.connect();
+        const userCollection = client.db("insertDB").collection("users");
+        const usersDB = await userCollection.find({}).toArray();
+
+
+        const foundUser = usersDB.find(person => person.username === user);
+
+        if (!foundUser) return res.sendStatus(401); //Unauthorized
+        // evaluate password 
+        const match = await bcrypt.compare(pass, foundUser.password);
+        if (match) {
+            // create JWTs
+            const accessToken = jwt.sign(
+                { "username": foundUser.username },
+                process.env.ACCESS_TOKEN_SECRET,
+                { expiresIn: '30s' }
+            );
+            const refreshToken = jwt.sign(
+                { "username": foundUser.username },
+                process.env.REFRESH_TOKEN_SECRET,
+                { expiresIn: '1d' }
+            );
+            // Saving refreshToken with current user
+            userCollection.updateOne({ "username": user }, { $set: { refreshToken: refreshToken } })
+            res.cookie('jwt', refreshToken, { httpOnly: true, sameSite: 'None', secure: true, maxAge: 24 * 60 * 60 * 1000 });
+            res.json({ accessToken });
+        } else {
+            res.sendStatus(401);
+        }
+    } catch (err) {
         res.sendStatus(401);
     }
+
+
+
+
 
 
 
--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,9 +1,8 @@
-const usersDB = {
-    users: require('../model/users.json'),
-    setUsers: function (data) { this.users = data }
-}
-const fsPromises = require('fs').promises;
-const path = require('path');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PWD}@cluster0.sn77ciu.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
 const bcrypt = require('bcrypt');
 
 const handleNewUser = async (req, res) => {
@@ -11,21 +10,24 @@ const handleNewUser = async (req, res) => {
     const { user, pass } = req.body;
     if (!user || !pass) return res.status(400).json({ 'message': 'Username and password are required.' });
     // check for duplicate usernames in the db
-    const duplicate = usersDB.users.find(person => person.username === user);
-    if (duplicate) return res.sendStatus(409); //Conflict
+    // const duplicate = usersDB.users.find(person => person.username === user);
+    // if (duplicate) return res.sendStatus(409); //Conflict
 
     try {
+        await client.connect();
+        const userCollection = client.db("insertDB").collection("users");
         //encrypt the password
         const hashedPwd = await bcrypt.hash(pass, 10);
         //store the new user
         const newUser = { "username": user, "password": hashedPwd };
-        usersDB.setUsers([...usersDB.users, newUser]);
-        await fsPromises.writeFile(
-            path.join(__dirname, '..', 'model', 'users.json'),
-            JSON.stringify(usersDB.users)
-        );
-        console.log(usersDB.users);
-        res.status(201).json({ 'success': `New user ${user} created!` });
+        const result = await userCollection.updateOne({ username: user }, { $setOnInsert: newUser }, { upsert: true })
+        // console.log(result);
+        if (result.matchedCount === 1) {
+            return res.sendStatus(409);
+        }
+        else {
+            res.status(201).json(result);
+        }
     } catch (err) {
         res.status(500).json({ 'message': err.message });
     }
