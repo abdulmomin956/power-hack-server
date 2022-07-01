f3a81fe7be744b99ea37c3a7d835648dd63ba0de
Abdul Momin <66585914+abdulmomin956@users.noreply.github.com>
2022-07-01 23:49:00 +0600

    refreshtoken with mongo db

--- controllers/refreshTokenController.js
+++ controllers/refreshTokenController.js
@@ -1,7 +1,7 @@
-const usersDB = {
-    users: require('../model/users.json'),
-    setUsers: function (data) { this.users = data }
-}
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PWD}@cluster0.sn77ciu.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
@@ -10,22 +10,32 @@ const handleRefreshToken = (req, res) => {
     if (!cookies?.jwt) return res.sendStatus(401);
     const refreshToken = cookies.jwt;
 
-    const foundUser = usersDB.users.find(person => person.refreshToken === refreshToken);
-    if (!foundUser) return res.sendStatus(403); //Forbidden 
+    try {
+        await client.connect();
+        const userCollection = client.db("insertDB").collection("users");
+        const usersDB = await userCollection.find({}).toArray();
+        const foundUser = usersDB.find(person => person.refreshToken === refreshToken);
+        if (!foundUser) return res.sendStatus(403); //Forbidden 
+        jwt.verify(
+            refreshToken,
+            process.env.REFRESH_TOKEN_SECRET,
+            (err, decoded) => {
+                if (err || foundUser.username !== decoded.username) return res.sendStatus(403);
+                const accessToken = jwt.sign(
+                    { "username": decoded.username },
+                    process.env.ACCESS_TOKEN_SECRET,
+                    { expiresIn: '30s' }
+                );
+                res.json({ accessToken })
+            }
+        );
+    } catch {
+
+    }
+
+
     // evaluate jwt 
-    jwt.verify(
-        refreshToken,
-        process.env.REFRESH_TOKEN_SECRET,
-        (err, decoded) => {
-            if (err || foundUser.username !== decoded.username) return res.sendStatus(403);
-            const accessToken = jwt.sign(
-                { "username": decoded.username },
-                process.env.ACCESS_TOKEN_SECRET,
-                { expiresIn: '30s' }
-            );
-            res.json({ accessToken })
-        }
-    );
+
 }
 
 module.exports = { handleRefreshToken }
